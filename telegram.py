5a29fcf888b06f4ac5e736e86fc780d82f50365a
Miguel Espina <miguelangel_43@outlook.com>
2021-03-25 11:51:14 +0100

    debugging

--- telegram.py
+++ telegram.py
@@ -97,10 +97,10 @@ class SyncTelegramClient:
                     # If a msg was forwarded from another channel, append it to the list
                     if m.fwd_from:
                         print(m.fwd_from)
-                        if hasattr(m.fwd_from ,'channel_id'):
-                            new_edges.append([group, m.fwd_from.channel_id])
-                            if m.fwd_from.channel_id not in new_groups:
-                                if m.fwd_from.channel_id not in visited_channels:
+                        if hasattr(m.fwd_from ,'from_id'):
+                            new_edges.append([group, m.fwd_from.from_id.channel_id])
+                            if m.fwd_from.from_id.channel_id not in new_groups:
+                                if m.fwd_from.from_id.channel_id not in visited_channels:
                                     new_groups.append(m.fwd_from.channel_id)
             # The channel contains less messages than BATCH_SIZE
             except BufferError:
