75df6dd7040bb10224b7928774f1b241d3339b81
Miguel Espina <miguelangel_43@outlook.com>
2021-04-17 19:05:42 +0200

    list error fixed

--- crawler.py
+++ crawler.py
@@ -27,8 +27,8 @@ model = Simple(telethon_api)
 
 """
 
-BATCH_SIZE = 100 # Number of messages that will be collected to search for mentions
-NUM_ITERATIONS = 1
+BATCH_SIZE = 1000 # Number of messages that will be collected to search for mentions
+NUM_ITERATIONS = 2
 QUERY = ['corona', 'covid', 'qanon', 'querdenk', 'giftspritz', 'impf'] # With this query words like 'coronavirus' or 'covid-19' will also count
 THRESHOLD_IN_PERCENTAGE = 0.75
 
@@ -37,15 +37,15 @@ seed_input = pd.read_csv('groups.csv')
 seed_input = seed_input.loc[(seed_input['consp'] == 1.0) & (seed_input['eng'] != 1.0)]
 seed_input = seed_input.drop(columns = ['consp', 'eng'])
 seed_input.reset_index(inplace=True)
-seed_input = seed_input['ch_id'].tolist()[:1]
+seed_input = seed_input['ch_id'].tolist()[:20]
 
 # Initializing variables
-seed = seed_input
-visited_channels = seed_input
-collected_channels = seed_input
-iteration_channels = seed_input # The output channels in iteration i will be the input channels of iteration i+1
+seed = list(seed_input)
+visited_channels = list(seed_input)
+collected_channels = list(seed_input)
+iteration_channels = list(seed_input) # The output channels in iteration i will be the input channels of iteration i+1
 
-# Initializing csv file. To be run only the first time, s.t. the csv file is created
+# Initializing csv file. To be run only the first time, s.t. the csv file is created with the proper column names
 # with open('crawls.csv', mode='w') as f:
 #     writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 #     writer.writerow(['Date', 'Crawler', 'Retrieved messages per group', 'Iteration', 'Total Iterations', 'Seed size', 'Query', 'Threshold',
@@ -56,19 +56,16 @@ ranked_seed, seed_score = model.rank(seed, QUERY)
 threshold = THRESHOLD_IN_PERCENTAGE * seed_score
 print('Seed scores:', seed_score)
 print(threshold, 'will be used as threshold')
-print(seed)
+
 for i in range(NUM_ITERATIONS):
     print('Iteration:', i)
     # Find groups given original seed or new channels
     print('Finding new channels..')
     found_channels = telethon_api.find_groups_fwd(visited_channels=visited_channels, old_groups = iteration_channels, batch_size=BATCH_SIZE)
-    print(seed)
-    visited_channels += found_channels # Error: seed is also extended
-    print(seed)
+    visited_channels.extend(found_channels) 
     # Rank the groups based on a QUERY
     print('Ranking channels..')
-    ranked_channels, avg_score = model.rank(found_channels, QUERY) # Error: seed is also extended
-    print(seed)
+    ranked_channels, avg_score = model.rank(found_channels, QUERY)
     # Add the highest ranked channels to the seed.
     print('Adding highest ranked channels to seed...') 
     iteration_channels = model.get_filtered_channels(ranked_channels, threshold)
@@ -77,24 +74,18 @@ for i in range(NUM_ITERATIONS):
     else:
         print('No new channels added')
     print('Average score:', avg_score)
-    collected_channels += iteration_channels
-    print(seed)
+    collected_channels.extend(iteration_channels)
     # Save the data in the csv file
-#     with open('crawls.csv', mode='a') as f:
-#         writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-#         writer.writerow([datetime.datetime.now(), model_name, BATCH_SIZE, i+1, NUM_ITERATIONS, len(seed), QUERY, threshold, 
-#         THRESHOLD_IN_PERCENTAGE, avg_score, seed, collected_channels, ranked_channels, iteration_channels])
-#     print(len(seed))
+    with open('crawls.csv', mode='a') as f:
+        writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        writer.writerow([datetime.datetime.now(), model_name, BATCH_SIZE, i+1, NUM_ITERATIONS, len(seed), QUERY, threshold, 
+        THRESHOLD_IN_PERCENTAGE, avg_score, seed, collected_channels, ranked_channels, iteration_channels])
 
 # # Save the collected channels and the seed in a csv file
-# with open('collected_channels_test.csv', 'w') as f:
-#     w = csv.writer(f)
-#     w.writerow(collected_channels)
-# with open('seed_test.csv', 'w') as f:
-#     w = csv.writer(f)
-#     w.writerow(seed)
+with open('collected_channels.csv', 'w') as f:
+    w = csv.writer(f)
+    w.writerow(collected_channels)
+with open('seed.csv', 'w') as f:
+    w = csv.writer(f)
+    w.writerow(seed)
 print(len(seed))
-
-# Evaluation
-#print('Evaluating..')
-#print('Rate of seed recollection:', evaluation.evaluate(seed, collected_channels, num_iterations=1)*100, '%')
